a7d337d64b23067832c89b5889608e420ecd5caf
AnthonyLaiuppa <anthonylaiuppa@gmail.com>
2017-07-07 16:07:54 -0400

    Initial Commit for new site project

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.template import Context,loader
+from datetime import datetime
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+	return render(request, 'index.html');
